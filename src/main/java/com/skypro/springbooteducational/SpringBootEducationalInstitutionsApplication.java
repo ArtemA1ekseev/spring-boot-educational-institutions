a374bff6f3d376ada319389f9ce31397654aea37
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-12-07 12:50:11 +0400

    initial commit.

---
+++ src/main/java/com/skypro/springbooteducational/SpringBootEducationalInstitutionsApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.springbooteducational;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEducationalInstitutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEducationalInstitutionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/springbooteducational/SpringBootEducationalInstitutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.springbooteducational;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEducationalInstitutionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
