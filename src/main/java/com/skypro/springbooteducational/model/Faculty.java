5ca6599fa0bff7c7ed85e55c64e708c8e56c227a
arste <a1ekseevartem.ru@gmail.com>
2022-12-14 22:26:57 +0400

    Testing web applications in Spring Boot. completed.

--- src/main/java/com/skypro/springbooteducational/model/Faculty.java
+++ src/main/java/com/skypro/springbooteducational/model/Faculty.java
@@ -2,17 +2,14 @@ package com.skypro.springbooteducational.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.Objects;
 
 @Entity
 public class Faculty{
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name, color;
