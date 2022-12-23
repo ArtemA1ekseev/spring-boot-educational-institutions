145088a5970f5bda20c0ec789e9a707bfb021a4f
arste <a1ekseevartem.ru@gmail.com>
2022-12-23 19:53:14 +0400

    Migrations and indexes. completed. fix.

--- src/main/resources/liquibase/scripts/course-four-lesson-three.sql
+++ src/main/resources/liquibase/scripts/course-four-lesson-three.sql
@@ -1,6 +1,7 @@
 -- liquibase formatted sql
 
---changeset aalekseev:1
-
+-- changeset aalekseev:1
 CREATE INDEX student_name_index ON student (name);
+
+-- changeset aalekseev:2
 CREATE INDEX faculty_nc_index ON faculty (name, color);
